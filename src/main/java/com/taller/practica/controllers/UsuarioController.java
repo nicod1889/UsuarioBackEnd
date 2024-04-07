d6e10e7e37178efbebfc4843b8c50a69f24f5e7a
Gabriel Pereyra <69758300+gabopereyra@users.noreply.github.com>
2024-04-07 13:05:00 -0300

    fix: remove unused import, change access modified (private to public) (#9)

--- src/main/java/com/taller/practica/controllers/UsuarioController.java
+++ src/main/java/com/taller/practica/controllers/UsuarioController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -29,7 +28,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    private ResponseEntity<Usuario> findById(@PathVariable Integer id) {
+    public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
         return usuarioRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
